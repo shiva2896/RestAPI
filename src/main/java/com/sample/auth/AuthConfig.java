9c53bad854f8e5f77fc50602f8f72ee5cbfeb7fa
shiva2896 <67514985+shiva2896@users.noreply.github.com>
2020-06-29 11:55:58 +0530

    Add files via upload

--- src/main/java/com/sample/auth/AuthConfig.java
+++ src/main/java/com/sample/auth/AuthConfig.java
@@ -1,1 +1,35 @@
+package com.sample.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AuthConfig extends WebSecurityConfigurerAdapter{
+
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	}
+  
+	 @Autowired
+	    public void configureGlobal(AuthenticationManagerBuilder authentication)
+	            throws Exception
+	    {
+	        authentication.inMemoryAuthentication()
+	                .withUser("shiva")
+	                .password(passwordEncoder().encode("Shivassy@28"))
+	                .authorities("ROLE_USER");
+	    }
+	 
+	 @Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
+}
