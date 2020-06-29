d860df0eaf8e59389220d4afe36c7edc00187c09
shiva2896 <67514985+shiva2896@users.noreply.github.com>
2020-06-29 11:57:22 +0530

    Add files via upload

--- src/main/java/com/sample/controller/Controller.java
+++ src/main/java/com/sample/controller/Controller.java
@@ -1,1 +1,28 @@
+package com.sample.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.dao.Request;
+import com.sample.dao.Response;
+
+@RestController
+@RequestMapping("/SimplePostAPI")
+public class Controller {
+
+	
+	@PostMapping(value = "/SimpleService", consumes = MediaType.APPLICATION_JSON_VALUE) 
+	public ResponseEntity<?>  simpleResponse(@RequestBody Request requestJson) {
+		Response response = new Response();
+		response.setName(requestJson.getName());
+		response.setStatus("SUCESS");
+	  return new  ResponseEntity<Response>(response,HttpStatus.OK) ;
+	  
+	  }
+	 
+}
