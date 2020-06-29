3eb85fce34698d58a096dfe63a4a583760988ce1
shiva2896 <67514985+shiva2896@users.noreply.github.com>
2020-06-29 11:54:33 +0530

    Add files via upload

--- src/main/java/com/sample/service/Application.java
+++ src/main/java/com/sample/service/Application.java
@@ -1,1 +1,16 @@
+package com.sample.service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(value={ "com.sample"} )
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 SpringApplication.run(Application.class, args);
+	}
+
+}
